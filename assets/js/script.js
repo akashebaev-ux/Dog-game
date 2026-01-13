3ab53eed9968422df1e99c1183076058bcbc0d4e
Azamat Kashebayev <AKashebaev@gmail.com>
2026-01-13 22:45:12 +0500

    "Add run and jump loops and canvas ctx"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-console.log("Hello, Dog Game!");
+//=== Image Preloading settings ===
+
+const images_RUN = 15;
+const images_Jump = 11;
+
+const playerWidth = 300;
+const playerHeight = 300;
+
+//=== Player Runs ===
+
+const playerRunImages = [];
+for (let i = 1; i <= images_RUN; i++) {
+    const img = new Image();
+    img.src = `assets/images/Run (${i}).png`;
+    playerRunImages.push(img);
+}
+
+//=== Player Jumps ===
+const playerJumpImages = [];
+for (let i = 1; i <= images_Jump; i++) {
+    const img = new Image();
+    img.src = `assets/images/Jump (${i}).png`;
+    playerJumpImages.push(img);
+}
+
+
+//=== Canvas ===
+const canvas = document.getElementById("backgroundCanvas");
+const ctx = canvas.getContext("2d");
